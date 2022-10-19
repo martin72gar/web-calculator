67b7e2a24a09173954c14ae66a765b4ac1e46de8
Martin (Office PC) <dev@fortyresources.com>
2022-10-19 15:56:47 +0700

    using localStorage webAPI

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -89,5 +89,14 @@ function performCalculation() {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
 
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+
+    putHistory(history);
     calculator.displayNumber = result;
+    renderHistory();
 }
